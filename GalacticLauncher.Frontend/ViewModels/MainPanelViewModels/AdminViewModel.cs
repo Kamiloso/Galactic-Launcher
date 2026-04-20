55a71142d96aafd4bd047d2baffd94ca9df26876
dorsz15 <fer.busysurvivinglife77@gmail.com>
2026-04-20 20:50:49 +0200

    small tweaks to ui

---
+++ GalacticLauncher.Frontend/ViewModels/MainPanelViewModels/AdminViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalacticLauncher.Frontend.ViewModels.MainWindowViewModels;
+using GalacticLauncher.Frontend.ViewModels.Interfaces;
+
+namespace GalacticLauncher.Frontend.ViewModels.MainPanelViewModels
+{
+    internal class AdminViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private readonly MainWindowViewModel _mainWindowViewModel;
+
+        public AdminViewModel(MainWindowViewModel mainWindowViewModel)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+        }
+        public void OnActivated()
+        {
+        }
+    }
+}
+
